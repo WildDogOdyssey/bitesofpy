36a77ad57b3fbb253c52edb852211a301ab60e6e
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-03-14 15:16:25 -0400

    submission Bite 246 @ codechalleng.es

---
+++ 246/test_workouts.py
@@ -0,0 +1,19 @@
+import pytest
+
+from workouts import print_workout_days
+
+
+def test_print_workout_days_2_days(capfd):
+    print_workout_days('lower')
+    output = capfd.readouterr()[0].strip()
+    assert output == 'Tue, Fri'
+
+def test_print_workout_days_no_match(capfd):
+    print_workout_days('squats')
+    output = capfd.readouterr()[0].strip()
+    assert output == 'No matching workout'
+
+def test_print_workout_days_4_days(capfd):
+    print_workout_days('body')
+    output = capfd.readouterr()[0].strip()
+    assert output == 'Mon, Tue, Thu, Fri'
