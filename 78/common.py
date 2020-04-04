fe29e0c8732b214d131242636acd121d6a7db39b
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-04-04 15:46:58 -0400

    submission Bite 78 @ codechalleng.es

--- 78/common.py
+++ 78/common.py
@@ -1,7 +1,9 @@
 def common_languages(programmers):
     """Receive a dict of keys -> names and values -> a sequence of
        of programming languages, return the common languages"""
-    lang_set = {language for programmer in programmers.values() for language in programmer}
-    for language in programmers.values():
-        lang_set = lang_set.intersection(language)
-    return lang_set
+    # lang_set = {language for programmer in programmers.values() for language in programmer}
+    # for language in programmers.values():
+    #     lang_set = lang_set.intersection(language)
+    # return lang_set
+
+    return set.intersection(*(set(languages) for languages in programmers.values()))
