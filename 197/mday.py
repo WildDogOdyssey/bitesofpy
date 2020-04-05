1f61ad82fdcd516a2d92469274040ffe97ce6529
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-04-05 16:15:59 -0400

    submission Bite 197 @ codechalleng.es

--- 197/mday.py
+++ 197/mday.py
@@ -1,9 +1,13 @@
 from datetime import date, timedelta
 
 
+MAY = 5
+SUNDAY = 6
+ONE_WEEK = 7
+
 def get_mothers_day_date(year):
     """Given the passed in year int, return the date Mother's Day
        is celebrated assuming it's the 2nd Sunday of May."""
-    first_day = date(year, 5, 1)
-    days_to_sunday = 6 - first_day.weekday()
-    return first_day + timedelta(days=(days_to_sunday+7))
+    first_day = date(year, MAY, 1)
+    days_to_sunday = SUNDAY - first_day.weekday()
+    return first_day + timedelta(days=(days_to_sunday+ONE_WEEK))
