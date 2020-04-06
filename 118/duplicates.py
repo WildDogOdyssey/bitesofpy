095c73fcfe0a6c6e6bf436ef90127bf8b245093a
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-04-06 16:16:29 -0400

    submission Bite 118 @ codechalleng.es

--- 118/duplicates.py
+++ 118/duplicates.py
@@ -10,10 +10,13 @@ def get_duplicate_indices(words):
        return [0, 1]:
        ['is', 'it', 'true', 'or', 'is', 'it', 'not?'] => [0, 1]
        Make sure the returning list is unique and sorted in ascending order."""
-    words = list(map(lambda word: word.lower(), words))
-    word_count = Counter(words)
-    repeated = dict(filter(lambda x: x[1] > 1, word_count.items()))
-    return [words.index(word) for word in repeated.keys()]
+    # words = list(map(lambda word: word.lower(), words))
+    # word_count = Counter(words)
+    # repeated = dict(filter(lambda x: x[1] > 1, word_count.items()))
+    # return [words.index(word) for word in repeated.keys()]
 
-# words = ['is', 'it', 'true', 'or', 'is', 'it', 'not']
-# print(get_duplicate_indices(words))
+    repeated = set(word for word in words if words.count(word) > 1)
+    return [words.index(word) for word in repeated]
+
+words = ['is', 'it', 'true', 'or', 'is', 'it', 'not']
+print(get_duplicate_indices(words))
