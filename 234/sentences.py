c990173a1b62934205b6bed1e558886cb5f15ca0
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-04-11 17:05:40 -0400

    submission Bite 234 @ codechalleng.es

--- 234/sentences.py
+++ 234/sentences.py
@@ -5,4 +5,4 @@ def capitalize_sentences(text: str) -> str:
     """Return text capitalizing the sentences. Note that sentences can end
        in dot (.), question mark (?) and exclamation mark (!)"""
     mo = re.findall(r'\w[^\.\?!]+[\.\?!]', text)
-    return ' '.join(line[0].upper() + line[1:] for line in mo)
+    return ' '.join(line.capitalize() for line in mo)
