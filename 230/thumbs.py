71c7cd1a43dd6e83da3bfce657d2fb97410f4588
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-04-29 15:40:08 -0400

    submission Bite 230 @ codechalleng.es

--- 230/thumbs.py
+++ 230/thumbs.py
@@ -3,19 +3,28 @@ THUMBS_UP, THUMBS_DOWN = '👍', '👎'
 
 class Thumbs:
 
-    def __call__(self, score):
-        # Raise ValueError if 0
+    def __mul__(self, score):
         if score == 0:
             raise ValueError
-        # Thumbs up or down based on sign
         thumb = THUMBS_UP if score > 0 else THUMBS_DOWN
-        # Long form representation
         num = abs(score)
-        rep = f' ({num}x)' if num > 3 else thumb*(num - 1)
-
-        return thumb + rep
-
-    def __mul__(self, other):
-        return self.__call__(other)
+        return (thumb + f' ({num}x)') if num > 3 else thumb*num
 
     __rmul__ = __mul__
+
+    # def __call__(self, score):
+    #     # Raise ValueError if 0
+    #     if score == 0:
+    #         raise ValueError
+    #     # Thumbs up or down based on sign
+    #     thumb = THUMBS_UP if score > 0 else THUMBS_DOWN
+    #     # Long form representation
+    #     num = abs(score)
+    #     rep = f' ({num}x)' if num > 3 else thumb*(num - 1)
+    #
+    #     return thumb + rep
+    #
+    # def __mul__(self, other):
+    #     return self.__call__(other)
+    #
+    # __rmul__ = __mul__
