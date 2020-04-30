9aa0f7284b5c40757be97b52f2f13823aff6c263
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-04-30 12:31:31 -0400

    submission Bite 245 @ codechalleng.es

---
+++ 245/improved_xmas_tree.py
@@ -0,0 +1,19 @@
+from math import floor
+
+
+STAR = "+"
+LEAF = "*"
+TRUNK = "|"
+
+
+def generate_improved_xmas_tree(rows=10):
+    """Generate a xmas tree with a star (+), leafs (*) and a trunk (|)
+       for given rows of leafs (default 10).
+       For more information see the test and the bite description"""
+    max_row = rows*2-1
+    star = [STAR.center(max_row)]
+    leaves = [((row*2-1)*LEAF).center(max_row) for row in range(1, rows + 1)]
+    trunks = [((round(max_row/2)+1)*TRUNK).center(max_row) for row in range(2)]
+    return '\n'.join(star + leaves + trunks)
+
+# print(generate_improved_xmas_tree())
