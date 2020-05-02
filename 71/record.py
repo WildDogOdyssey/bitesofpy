6b1de542949693b6eb9a17d8538c8a9e70c2f62b
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-05-02 12:38:32 -0400

    submission Bite 71 @ codechalleng.es

--- 71/record.py
+++ 71/record.py
@@ -1,18 +1,27 @@
 class RecordScore():
     """Class to track a game's maximum score"""
 
-    highest_score = None
+    # highest_score = None
+    #
+    # def __call__(self, score):
+    #     if self.highest_score == None:
+    #         self.highest_score = score
+    #     elif score > self.highest_score:
+    #         self.highest_score = score
+    #     return self.highest_score
+
+    def __init__(self):
+        self.highest_score = float('-inf')
 
     def __call__(self, score):
-        if self.highest_score == None:
-            self.highest_score = score
-        elif score > self.highest_score:
+        if self.highest_score < score:
             self.highest_score = score
         return self.highest_score
 
-# record = RecordScore()  # init
-# print(record(10))  # call
-# print(record(9))
-# print(record(11))
-# record = RecordScore()
+record = RecordScore()  # init
+print(record(10))  # call
+print(record(9))
+print(record(11))
+record = RecordScore()
 print(record(-2))
+print(record(-1))
