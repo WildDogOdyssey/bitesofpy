29932c907e9e05b4f3b54aab2f74a48cf5d04e21
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-05-06 11:59:42 -0400

    submission Bite 70 @ codechalleng.es

--- 70/eggs.py
+++ 70/eggs.py
@@ -6,10 +6,20 @@ COLORS = 'red blue green yellow brown purple'.split()
 class EggCreator:
 
     def __init__(self, limit):
-        self.egg_it = (choice(COLORS) for _ in range(limit))
+        self.limit = limit
+        self.count = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        return next(self.egg_it)
+        if self.count < self.limit:
+            self.count += 1
+            return choice(COLORS)
+        else:
+            raise StopIteration
+
+# eg = EggCreator(2)
+# print(next(eg))
+# print(next(eg))
+# print(next(eg))
