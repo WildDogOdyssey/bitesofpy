7d138c840aa9643c1fa3feb9124cd58d5d9dcd62
WildDogOdyssey <43459580+WildDogOdyssey@users.noreply.github.com>
2020-05-08 16:33:43 -0400

    submission Bite 82 @ codechalleng.es

---
+++ 82/score.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+THUMBS_UP = '👍'  # in case you go f-string ...
+
+# move these into an Enum:
+class Score(Enum):
+    BEGINNER = 2
+    INTERMEDIATE = 3
+    ADVANCED = 4
+    CHEATED = 1
+
+    def __str__(self):
+        return f'{self.name} => {self.value*THUMBS_UP}'
+
+    @classmethod
+    def average(cls):
+        values = [val.value for val in cls.__members__.values()]
+        return sum(values) / len(values)
